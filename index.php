f75bf0c4bfd73fc8c9f2d6dcc0b52105f5b281a9
yogeesh19 <72991792+yogeesh19@users.noreply.github.com>
2020-10-18 14:36:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<h1> THis is the AWS ElasticBEanstalk PHP code Deployment in the AWS </h1>
+
+<?php
+echo "Hi this is the  app THis is the AWS ElasticBEanstalk PHP code Deployment in the AWS";
+?>
+
+</html>
+</body>
+"Hi this is the sample PHP web ap"
